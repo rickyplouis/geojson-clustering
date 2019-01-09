11f9b5b5e82161c0cfc07090cafbb5c6b88ee03f
rickyplouis <ricardoplouis@gmail.com>
2019-01-09 16:44:20 -0600

    Puts each dp in nearest centroid

--- addresses.js
+++ addresses.js
@@ -6,11 +6,6 @@ const minLat = 0;
 const minLng = 0;
 let data = [];
 
-const euclideanDistance = (coordA, coordB) => {
-  return Math.sqrt(Math.pow(coordA.lat - coordB.lat, 2) + Math.pow(coordA.lng - coordB.lng, 2))
-}
-
-
 for (var x = 0; x < n; x++) {
   data.push({lat: getRandomInt(minLat, maxLat), lng: getRandomInt(minLng, maxLng)})
 }
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const k = 2;
 const data = require('./addresses');
 const getRandomInt = require('./getRandomInt');
 
+const euclideanDistance = (coordA, coordB) => {
+  return Math.sqrt(Math.pow(coordA.lat - coordB.lat, 2) + Math.pow(coordA.lng - coordB.lng, 2))
+}
+
 const getMin = (coords, prop) => {
   return coords.reduce((min, acc) => acc[prop] < min ? acc[prop] : min, coords[0][prop])
 }
@@ -23,4 +27,34 @@ const initializeCentroids = (n, data) => {
   return centroids;
 }
 
-console.log('initializeCentroids', initializeCentroids(k, data));
+const makeClusters = (centroids, data) => {
+  let clusters = [];
+  for (let n = 0; n < centroids.length; n += 1) {
+    clusters.push([]);
+  }
+  for (let x = 0; x < data.length; x += 1) {
+    let minCentroidIndex = 0;
+    for (let y = 0; y < centroids.length; y += 1) {
+      //change index of closest centroid
+      if (euclideanDistance(data[x], centroids[minCentroidIndex]) > euclideanDistance(data[x], centroids[y])) {
+        minCentroidIndex = y;
+      }
+      // reached end of array, put data point in cluster of closest centroid
+      if (y === centroids.length - 1 ) {
+        clusters[minCentroidIndex].push(data[x]);
+      }
+    }
+  }
+  return clusters;
+  // TODO
+  // for each datapoint
+  // for each centroid
+  // if distance(dpX, c) < distance(dPX, c)
+  // minCentroid = c
+  // after all iteration assigned dp to indexOf(centroid)
+}
+
+let nCentroids = initializeCentroids(k, data);
+let testClusters = makeClusters(nCentroids, data);
+console.log('nCentroids', nCentroids);
+console.log('testClusters', testClusters);
